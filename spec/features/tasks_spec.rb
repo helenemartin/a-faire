6c6594ba7df75513c21b4fac7cc0104dd571e38b
Helene <helenemartin13@hotmail.com>
2018-03-07 20:35:57 +0000

    create a features folder to move the task_spec file, correct deprecation rspec syntax, implement a form to create new tasks

---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "Tasks" do
+  describe "GET /tasks" do
+    it "display some task" do
+      @task = Task.create :task => 'go to the shop'
+      visit tasks_path
+      expect(page).to have_content 'go to the shop'
+    end
+    
+    it "creates a new task" do
+      visit tasks_path
+      fill_in 'Task', :with => 'go to work'
+      click_button 'Create Task'
+      expect(current_path).to eq(tasks_path)
+      expect(page).to have_content 'go to work'
+    end
+  end
+end
