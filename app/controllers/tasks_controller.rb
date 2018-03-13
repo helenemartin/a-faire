1c8e73a5d7014f647419660745f9fe6fdfa90de1
Helene <helenemartin13@hotmail.com>
2018-03-13 14:54:05 +0000

    add flash notice for task addition and completion, add styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,19 +2,22 @@ class TasksController < ApplicationController
   def index
     @pending_tasks = Task.where(status: "pending")
     @completed_tasks = Task.where(status: "completed")
+    
   end
   
   def create
     attributes = task_params
     attributes[:status] = "pending"
     Task.create attributes
-    redirect_to :back, :notice => 'task added'
+    flash[:notice] = "Task successfully created"
+    redirect_to :back
   end
   
   def destroy
     task = Task.find(params[:id])
     task.status = "completed"
     task.save
+    flash[:notice] = "Task successfully completed"
     redirect_to tasks_path
   end
   
